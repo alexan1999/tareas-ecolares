aa302671d9f7701e7811dd8d0392992d453dea19
alexander mendez <pabloalex1999abril@gmail.com>
2026-03-05 12:34:52 -0500

    subiendo tablas y controladores

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // Rutas
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/periodos', require('./routes/periodos.routes'));
