488bfe105750342db97939ff2523300f9618cbb7
alexander mendez <pabloalex1999abril@gmail.com>
2026-03-09 12:55:58 -0500

    subiendo tablas y controladores

--- backend/src/routes/materias.routes.js
+++ backend/src/routes/materias.routes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const verificarToken = require('../middlewares/auth.middleware');
+const controller = require('../controllers/materias.controller');
 
-// Ejemplo de ruta protegida
-router.get('/', verificarToken, (req, res) => {
-  res.json({
-    message: 'Ruta protegida',
-    usuario: req.usuario
-  });
-});
+// Crear materia
+router.post('/', verificarToken, controller.crearMateria);
+
+// Listar todas las materias
+router.get('/', verificarToken, controller.obtenerTodasLasMaterias);
+
+// Listar materias por periodo
+router.get('/:id_periodo', verificarToken, controller.obtenerMateriasPorPeriodo);
+
+// Obtener detalle de una materia
+router.get('/detalle/:id', verificarToken, controller.obtenerMateriaPorId);
+
+// Actualizar materia
+router.put('/:id', verificarToken, controller.actualizarMateria);
+
+// Eliminar materia
+router.delete('/:id', verificarToken, controller.eliminarMateria);
 
 module.exports = router;
--- backend/src/routes/tareas.routes.js
+++ backend/src/routes/tareas.routes.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const { getTareas } = require('../controllers/tareas.controller');
+const verificarToken = require('../middlewares/auth.middleware');
+const controller = require('../controllers/tareas.controller');
+/*
+Endpoints para tareas:
+localhost:3000/api/tareas
+http://localhost:3000/api/tareas/ metodos: post
+https://localhost:3000/api/tareas/ metodos: get
+http://localhost:3000/api/tareas/:id metodos: get
+http://localhost:3000/api/tareas/:id metodos: put
+http://localhost:3000/api/tareas/:id/completar metodos: patch
+http://localhost:3000/api/tareas/ metodos: delete
 
-// Definimos la ruta GET para obtener las tareas
-router.get('/', getTareas);
+*/
+//- POST /api/tareas: Crear una nueva tarea.
+
+router.post('/', verificarToken, controller.crearTarea);
+
+//- GET /api/tareas: Listar todas las tareas del usuario.
+router.get('/', verificarToken, controller.obtenerTodasLasTareas);
+
+//- GET /api/tareas/:id: Obtener detalle de una tarea específica.
+router.get('/:id', verificarToken, controller.obtenerTareaPorId);
+
+//- PUT /api/tareas/:id: Actualizar una tarea.
+router.put('/:id', verificarToken, controller.actualizarTarea);
+
+//- PATCH /api/tareas/:id/completar: Marcar una tarea como completada.
+router.patch('/:id/completar', verificarToken, controller.marcarComoCompletada);
+
+//- DELETE /api/tareas/:id: Eliminar una tarea.
+router.delete('/:id', verificarToken, controller.eliminarTarea);
+
+
+//funcionalidades adicionales de tareas para filtrar por estado
+router.get('/estado/pendientes', verificarToken, controller.tareasPendientes);
+router.get('/estado/vencidas', verificarToken, controller.tareasVencidas);
+router.get('/estado/completadas', verificarToken, controller.tareasCompletadas);
+
 
 module.exports = router;
+
+
